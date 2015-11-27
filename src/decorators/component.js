867f7098a64a90468f73ee626dae93b31f4e784e
Andrii Gikalo <a.gikalo@plarium.com>
2015-11-27 16:01:02 +0200

    remove duplicated code

--- src/decorators/component.js
+++ src/decorators/component.js
@@ -1,23 +1,5 @@
-import isFunction from 'lodash/lang/isFunction';
-import endsWith   from 'lodash/string/endsWith';
-import capitalize from 'lodash/string/capitalize';
-
-import className from '../utils/classname';
-
-const CONTROLLER_NAME_SUFFIX = 'controller';
+import { getControllerName } from './controller';
 
 export default options => target => {
-  if (!isFunction(target)) {
-    throw new TypeError('Component must be a class');
-  }
-  
-  let constructorName = className(target);
-  
-  if (!endsWith(constructorName, CONTROLLER_NAME_SUFFIX)) {
-    throw new Error(`Controller name must ends with ${capitalize(CONTROLLER_NAME_SUFFIX)}`);
-  }
-  
-  let controllerName = constructorName.slice(0, CONTROLLER_NAME_SUFFIX.length);
-  
-  valent.controller(controllerName, target, options);
+  valent.component(getControllerName(target), target, options);
 };
--- src/decorators/controller.js
+++ src/decorators/controller.js
@@ -1,12 +1,10 @@
 import isFunction from 'lodash/lang/isFunction';
 import endsWith   from 'lodash/string/endsWith';
-import capitalize from 'lodash/string/capitalize';
+import className  from '../utils/classname';
 
-import className from '../utils/classname';
+const CONTROLLER_NAME_SUFFIX = 'Controller';
 
-const CONTROLLER_NAME_SUFFIX = 'controller';
-
-export default options => target => {
+export const getControllerName = target => {
   if (!isFunction(target)) {
     throw new TypeError('Controller must be a class');
   }
@@ -14,10 +12,12 @@ export default options => target => {
   let constructorName = className(target);
   
   if (!endsWith(constructorName, CONTROLLER_NAME_SUFFIX)) {
-    throw new Error(`Controller name must ends with ${capitalize(CONTROLLER_NAME_SUFFIX)}`);
+    throw new Error(`Controller name must ends with ${CONTROLLER_NAME_SUFFIX}`);
   }
   
-  let controllerName = constructorName.slice(0, CONTROLLER_NAME_SUFFIX.length);
-  
-  valent.controller(controllerName, target, options);
+  return constructorName.slice(0, CONTROLLER_NAME_SUFFIX.length).toLowerCase();
+};
+
+export default options => target => {
+  valent.controller(getControllerName(target), target, options);
 };
